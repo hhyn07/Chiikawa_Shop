6859f71628c20fb47a1799a6784297f1752fdd50
Ruby0710 <hhyn0710@gmail.com>
2024-12-28 18:02:38 +0800

    feat: 新增增加商品功能。新增魔法少女吉伊在images目錄。

--- pages/product_view.php
+++ pages/product_view.php
@@ -8,20 +8,44 @@ $id = $params[0];
 // 資料庫查詢獲取 name 和 price
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT 商品名稱,價格 FROM 商品";
-$echo = "";
-$result = $conn->query($sql);
-
 $name = "";
 $price = 0;
+$image_path = "";
 
-
-
+$sql = "SELECT * FROM `商品` WHERE `商品編號`=" . $id;
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+if ($row) {
+  $name = $row["商品名稱"];
+  $price = $row["價格"];
+  $image_path = $row["圖片路徑"];
+  $inventory = $row["商品庫存"];
+  $description = $row["商品描述"];
+} else {
+  echo "沒有這個產品";
+  header("Location: /Chiikawa_Shop/404");
+}
 ?>
 <!-- 到時候新增商品的時候，應該會多一個params的變數導到不同的商品 -->
-<h1>product <?php echo $params[0]; ?></h1>
+<div class="container my-5">
+  <div class="row align-items-start">
+    <!-- 圖片區 -->
+    <div class="col-md-6">
+      <img src="<?php echo htmlspecialchars("/Chiikawa_Shop" . $image_path); ?>" alt="Product Image" class="img-fluid"
+        style="width: 400px; height: auto;">
+    </div>
+    <!-- 文字區 -->
+    <div class="col-md-6">
+      <h2 class="display-4 text-dark"><?php echo htmlspecialchars($name); ?></h2>
+      <p class="text-danger">NT$<?php echo htmlspecialchars($price); ?></p>
+      <p class="text-danger">現庫存剩下 <?php echo htmlspecialchars($inventory); ?> 件</p>
+      <p class="text-muted"><?php echo htmlspecialchars($description); ?></p>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
