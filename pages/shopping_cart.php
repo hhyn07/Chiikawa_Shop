5d392c7c0086ae5d6d770ea9f8045320d162bcd1
yuto0226 <wayne71112@gmail.com>
2025-01-02 03:47:19 +0800

    refactor: 用 card 重新設計購物車顯示

--- pages/shopping_cart.php
+++ pages/shopping_cart.php
@@ -16,28 +16,51 @@ $sql = "SELECT * FROM `會員` WHERE `會員編號`= $id";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 if ($row) {
-    $sql = "SELECT * FROM `購物車` WHERE `會員編號`= $id";
+    $sql = "SELECT * FROM `購物車` JOIN `商品` WHERE `購物車`.`商品編號` = `商品`.`商品編號` AND `購物車`.`會員編號` = $id";
     $result = $conn->query($sql);
-    if($result){
-        echo "<div class='table-container'>";
-        echo "<table>";
-        echo "<tr><th>商品</th><th>圖片</th><th>數量</th><th>價格</th></tr>";    
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["商品編號"]. "</td><td>"; ?> 
-        <img src="<?php echo htmlspecialchars("/Chiikawa_Shop" . $row["圖片"]); ?>" alt="Product Image" class="img-fluid"
-        style="width: 200px; height: auto;">  
-        <?php echo "</td><td>" . $row["數量"]. "</td><td>" . $row["數量"] * $row["價格"]. "</td></tr>";
-    }
-    echo "</table>";
-    ?>
-    <form id="order" action="/Chiikawa_Shop/order" method="POST" style="margin-top: 20px;">
-        <input type="hidden" name="id" value="<?php echo htmlspecialchars($id); ?>">
-        <button type="submit" id="myButton" class="btn btn-custom">結帳</button>
-    </form>
+    if($result){   
+?>
+<div class="my-5 row d-flex align-items-stretch">
+    <div class="col col-xl-8 mx-xl-auto mx-sm-3 mx-3">
+        <div class="card shadow-sm">
+            <div class="card-body">
+        <?php
+        $total = 0;
+        $first = true;
+        while ($row = $result->fetch_assoc()) {
+                if(!$first)
+                    echo "<hr>";
+                $first = false;
+        ?>
+                <div class="d-flex">
+                    <span class="my-auto mx-4"><?php echo $row["商品編號"];?></span>
+                    <img class="object-fit-cover" style="max-height: 150px;" src="<?php echo htmlspecialchars("/Chiikawa_Shop" . $row["圖片"]); ?>" alt="">
+                    <span class="my-auto">
+                        商品名稱：<?php echo $row["商品名稱"]; ?><br>
+                        商品數量：<?php echo $row["數量"]; ?><br>
+                    </span>
+                    <span class="ms-auto my-auto">項目金額：<?php echo $row["數量"] * $row["價格"]?></span>
+                </div>
+        <?php
+
+            $total += $row["數量"] * $row["價格"];
+        }
+        ?>
+            </div>
+            <div class="card-footer d-flex">
+                <span class="ms-auto h4">總價格：<?php echo $total;?></span>
+            </div>
+        </div>
     </div>
+    <div class="col-md-12 mt-4 d-flex">
+        <form class="mx-auto mb-4" id="order" action="/Chiikawa_Shop/order" method="POST">
+            <input type="hidden" name="id" value="<?php echo htmlspecialchars($id); ?>">
+            <button class="btn btn-custom" type="submit" id="myButton">結帳</button>
+        </form>
+    </div>
+</div>
     <?php
-    }
-    else{
+    } else{
         echo "<h2>購物車中沒有商品</h2><br>";
         echo "<h2><a href='http://localhost/Chiikawa_Shop/product/list'>去購物</a></h2>";
     }
@@ -84,10 +107,6 @@ table td {
 
 table tr {
     background-color: #ffe4e1;
-}
-
-form {
-    margin-top: 20px; /* 按钮与表格的间距 */
 }
 
 .btn-custom {
