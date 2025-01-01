49c6b5f4ec5fc7757ca3f13be192b70564bdf77c
Ann <star123joker@gmail.com>
2025-01-01 18:36:24 +0800

    changes:更新資料庫結構 將下單日期改為現在時間

--- test/data.sql
+++ test/data.sql
@@ -25,7 +25,7 @@ CREATE TABLE `商品` (
 --
 
 INSERT INTO `商品` (`商品名稱`, `商品描述`, `商品庫存`, `價格`, `會員編號`, `圖片路徑`, `商品編號`) VALUES
-('魔法少女一代-烏薩奇', '☀商品名稱：吉伊卡哇 一代-魔法少女掛件\r\n☀尺寸：約 H170 x W140 x D80mm\r\n☀材質：聚酯纖維、鐵', '10', 540, 1, '/assets/images/magic_usagi.jpg', 1),
+('魔法少女一代-烏薩奇', '☀商品名稱：吉伊卡哇一代-魔法少女掛件<br>☀尺寸：約 H170 x W140 x D80mm<br>☀材質：聚酯纖維、鐵', '10', 540, 1, '/assets/images/magic_usagi.jpg', 1),
 ('魔法少女一代-吉伊卡哇', '☀商品名稱：吉伊卡哇 一代-魔法少女掛件\r\n☀尺寸：約 H170 x W140 x D80mm\r\n☀材質：聚酯纖維、鐵', '10', 540, 1, '/assets/images/magic_chii.jpg', 2);
 
 -- --------------------------------------------------------
@@ -60,9 +60,9 @@ INSERT INTO `會員` (`帳號`, `密碼`, `會員編號`, `姓名`, `生日`, `�
 --
 
 CREATE TABLE `訂單` (
-  `訂單編號` 	int(100) NOT NULL,
-  `運費` 	int(100) DEFAULT NULL,
-  `總價格` 	int(100) DEFAULT NULL,
+  `訂單編號` int(100) NOT NULL,
+  `運費` int(100) DEFAULT NULL,
+  `總價格` int(100) DEFAULT NULL,
   `郵遞區號` char(100) DEFAULT NULL,
   `運送方式` char(100) DEFAULT NULL,
   `運送國家` char(100) DEFAULT NULL,
@@ -74,9 +74,9 @@ CREATE TABLE `訂單` (
   `商品編號` int(100) DEFAULT NULL,
   `商品名稱` char(100) DEFAULT NULL,
   `商品數量` char(100) DEFAULT NULL,
-  `訂單狀態` char(100) DEFAULT NULL,
-  `付款狀態` char(100) DEFAULT NULL,
-  `下單日期` char(100) DEFAULT NULL,
+  `訂單狀態` char(100) DEFAULT '訂單成立',
+  `付款狀態` char(100) DEFAULT '未付款',
+  `下單日期` timestamp(5) NULL DEFAULT current_timestamp(5),
   `預計到貨日期` char(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -87,7 +87,7 @@ CREATE TABLE `訂單` (
 --
 
 CREATE TABLE `購物車` (
-  `商品編號` int(100) DEFAULT NULL,
+  `商品編號` int(100) NOT NULL,
   `圖片` char(100) DEFAULT NULL,
   `會員編號` int(100) NOT NULL,
   `數量` char(100) DEFAULT NULL,
@@ -99,18 +99,18 @@ CREATE TABLE `購物車` (
 --
 
 --
--- 資料表索引 `會員`
---
-ALTER TABLE `會員`
-  ADD PRIMARY KEY (`會員編號`),
-  ADD KEY `idx_會員_姓名` (`姓名`,`電話`,`電郵`);
-
---
 -- 資料表索引 `商品`
 --
 ALTER TABLE `商品`
   ADD PRIMARY KEY (`商品編號`),
   ADD KEY `商品_ibfk_1` (`會員編號`);
+
+--
+-- 資料表索引 `會員`
+--
+ALTER TABLE `會員`
+  ADD PRIMARY KEY (`會員編號`),
+  ADD KEY `idx_會員_姓名` (`姓名`,`電話`,`電郵`);
 
 --
 -- 資料表索引 `訂單`
@@ -126,7 +126,7 @@ ALTER TABLE `訂單`
 -- 資料表索引 `購物車`
 --
 ALTER TABLE `購物車`
-  ADD PRIMARY KEY (`會員編號`, `商品編號`),
+  ADD PRIMARY KEY (`會員編號`,`商品編號`),
   ADD KEY `購物車_商品編號_fk` (`商品編號`);
 
 --
@@ -143,7 +143,13 @@ ALTER TABLE `商品`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `會員`
 --
 ALTER TABLE `會員`
-  MODIFY `會員編號` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `會員編號` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `訂單`
+--
+ALTER TABLE `訂單`
+  MODIFY `訂單編號` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 已傾印資料表的限制式
@@ -167,4 +173,3 @@ ALTER TABLE `訂單`
 ALTER TABLE `購物車`
   ADD CONSTRAINT `購物車_商品編號_fk` FOREIGN KEY (`商品編號`) REFERENCES `商品` (`商品編號`),
   ADD CONSTRAINT `購物車_會員編號_fk` FOREIGN KEY (`會員編號`) REFERENCES `會員` (`會員編號`);
---
